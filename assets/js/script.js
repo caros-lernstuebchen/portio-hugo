10af2418e399ae35b8327ce399100c85fc98d507
Peter Motzko <info@pmoscode.de>
2024-08-05 23:06:45 +0200

    refactor: replace old smooth scroll with smoothScroll library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,3 @@
-// Preloader
-
-function PageLoad() {
-  $("body").removeClass("hidden");
-  TweenMax.to($(".preloader-text"), 1, {
-    force3D: true,
-    opacity: 1,
-    y: 0,
-    delay: 0.2,
-    ease: Power3.easeOut,
-  });
-
-  var width = 100,
-    perfData = window.performance.timing,
-    EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
-    time = parseInt((EstimatedTime / 500) % 50) * 70;
-
-  // Percentage Increment Animation
-  var PercentageID = $("#precent"),
-    start = 1,
-    end = 100,
-    durataion = time;
-  animateValue(PercentageID, start, end, durataion);
-
-  function animateValue(id, start, end, duration) {
-    var range = end - start,
-      current = start,
-      increment = end > start ? 1 : -1,
-      stepTime = Math.abs(Math.floor(duration / range)),
-      obj = $(id);
-
-    var timer = setInterval(function () {
-      current += increment;
-      $(obj).text(current);
-      if (current === end) {
-        clearInterval(timer);
-      }
-    }, stepTime);
-  }
-  // Fading Out Loadbar on Finised
-  setTimeout(function () {
-    TweenMax.to($(".percentage, .inner"), 0.7, {
-      force3D: true,
-      opacity: 0,
-      yPercent: -101,
-      ease: Power3.easeInOut,
-    });
-    TweenMax.to($(".preloader-wrap"), 0.7, {
-      force3D: true,
-      yPercent: -150,
-      delay: 0.2,
-      ease: Power3.easeInOut,
-    });
-  }, time);
-}
 $(document).ready(function () {
   // preloder
   PageLoad();
@@ -67,18 +12,10 @@ $(document).ready(function () {
   });
 
   // Smooth scrolling
-  var scrollLink = $(".scroll");
-  scrollLink.click(function (e) {
-    let elem = $(this.hash);
-    if (elem.length) {
-      e.preventDefault();
-      $("body,html").animate(
-        {
-          scrollTop: elem.offset().top,
-        },
-        1000
-      );
-    }
+  new SmoothScroll('a[class*="scroll"]', {
+    easing: 'easeInQuad',
+    speed: 300,
+    header: 'nav'
   });
 
   $(".navbar-nav>li>a").on("click", function () {
